f97769c502d9081c85e1da05350f0340bb2b3430
Mvdv00 <matthijsvorm@gmail.com>
2026-01-20 20:19:45 +0100

    fixed the tests

--- v1/Database/database_creation.py
+++ v1/Database/database_creation.py
@@ -82,6 +82,7 @@ def create_database(db_path="v1/Database/MobyPark.db"):
             user_id INTEGER NOT NULL,
             vehicle_id INTEGER,
             started TEXT NOT NULL,
+            stopped TEXT,
             duration_minutes INTEGER NOT NULL,
             payment_status TEXT NOT NULL,
             FOREIGN KEY (parking_lot_id) REFERENCES parking_lots(id) ON DELETE CASCADE,
